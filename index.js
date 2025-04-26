d5f3461a40c88a8dc25059fb9dfdcb0c5b066d24
Daksh Jain <jain.daksh0808@gmail.com>
2025-04-26 19:51:08 +0530

    sending custom 404

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+app.use("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the Task API" });
+})
 app.use("/tasks", router);
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
